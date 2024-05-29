633ae2aa933fc4e4f4af5e9e6fe8297ff576fc92
Thomas Rohloff <v10lator@myway.de>
2024-05-29 20:09:54 +0200

    v1.151-BETA1
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- include/utils.h
+++ include/utils.h
@@ -29,7 +29,7 @@
 #include <coreinit/mcp.h>
 #pragma GCC diagnostic pop
 
-#define NUSSPLI_VERSION            "1.150"
+#define NUSSPLI_VERSION            "1.151-BETA1"
 
 #define NAPI_URL                   "https://napi.v10lator.de/v2/"
 #define NUSSPLI_COPYRIGHT          "© 2020-2024 V10lator <v10lator@myway.de>"
